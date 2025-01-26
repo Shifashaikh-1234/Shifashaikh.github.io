ef4f725b49c9bc37d52b965b90d702616b08435b
Shifa Tazeen <shifatazeen.msc@gmail.com>
2025-01-26 10:44:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 
 import "./App.scss";
-import { Routes, Route} from "react-router-dom";
-import ParticlesComponent from "./utils.js/particles";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 import Home from "./containers/home";
 import About from "./containers/about";
+import Resume from "./containers/resume";
 import Skills from "./containers/skills";
 import Portfolio from "./containers/portfolio";
 import Contact from "./containers/contact";
 import Navbar from "./components/navBar";
+import particles from "./utils.js/particles";
 
 function App() {
+  const location = useLocation();
+  console.log(location);
 
+  const handleInit = async (main) => {
+    await loadFull(main);
+  };
 
-return (
+  const renderParticleJsInHomePage = location.pathname === "/";
+
+  return (
     <div className="App">
       {/* particles js */}
 
-      
-        <ParticlesComponent id="particles" />
-      
+      {renderParticleJsInHomePage && (
+        <Particles id="particles" options={particles} init={handleInit} />
+      )}
 
       {/* navbar */}
       <Navbar />
@@ -28,6 +38,7 @@ return (
       <Routes>
         <Route index path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<Contact />} />
